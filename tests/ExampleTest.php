bcd18c8d292351365a612696492cfefdcf829ad9
Joe Scott <jscott@rawnet.com>
2023-12-07 18:00:06 +0000

    Initial test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,13 @@
 <?php
 
+use Silo\StorageConnectors\Factories\StorageConnectorFactory;
+
 it('can test', function () {
-    expect(true)->toBeTrue();
+    $gDrive = StorageConnectorFactory::create('google_drive');
+
+    $file = $gDrive->get('FILE ID');
+    expect($file->id)->not()->toBeNull();
+
+    $files = $gDrive->list(null, 5);
+    expect($files)->not()->toBeEmpty();
 });
